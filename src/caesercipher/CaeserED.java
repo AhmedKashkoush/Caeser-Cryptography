c4202d934b5a81d7e77bc2d9c05ebfd6a00dfedc
Ahmed Kashkoush <ahmedkashkoush3@gmail.com>
2021-12-27 17:46:52 +0200

    Start Caeser

--- src/caesercipher/CaeserED.java
+++ src/caesercipher/CaeserED.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package caesercipher;
 
-/**
- *
- * @author Osama
- */
 public class CaeserED {
 
     private boolean encrypted = false;
@@ -41,22 +32,29 @@ public class CaeserED {
             }
             this.cipherText = "";
             encrypted = false;
+        } else {
+            throw new Exception("There is no encrypted text,use encrypt() method to encrypt new one");
         }
-        else throw new Exception("There is no encrypted text,use encrypt() method to encrypt new one");
     }
 
     private int shift(char letter, int key) {
         int newLetter = (int) letter;
         return newLetter + key;
     }
-    
-    public String getPlain() throws Exception{
-        if (!this.plainText.isEmpty()) return this.plainText;
-        else throw new Exception("There is no plain text");
+
+    public String getPlain() throws Exception {
+        if (!this.plainText.isEmpty()) {
+            return this.plainText;
+        } else {
+            throw new Exception("There is no plain text");
+        }
     }
-    
-    public String getCipher() throws Exception{
-        if (!this.cipherText.isEmpty()) return this.cipherText;
-        else throw new Exception("There is no cipher text");
-    }    
+
+    public String getCipher() throws Exception {
+        if (!this.cipherText.isEmpty()) {
+            return this.cipherText;
+        } else {
+            throw new Exception("There is no cipher text");
+        }
+    }
 }
